8e0374857f907e5c76ca7313d1bf9a00da8ec770
Deobrat QMatX <137123400+deobratsingh9@users.noreply.github.com>
2026-02-14 03:45:54 +0100

    Add files via upload
    STM Simulation in VASP
    
    This repository provides a workflow to simulate STM images using VASP. It covers both constant-height and constant-current modes, including simulations at different tip heights and bias voltages.
    
    Features
    
    Constant-Height & Constant-Current STM simulations
    
    Bias-dependent filled/empty state images
    
    Partial charge density extraction (LDOS)
    
    Example surface slabs and INCAR templates
    
    Post-processing scripts for visualization
    
    Usage
    
    Prepare surface slab (POSCAR, KPOINTS, POTCAR)
    
    Set INCAR for STM mode
    
    Run VASP to get partial charge density
    
    Post-process to generate STM images
    
    Adjust tip height and bias for desired contrast
    
    Resources
    Tutorial Video: STM Simulation in VASP: Constant-Height & Constant-Current Modes Explained

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J pdte2
-#SBATCH -N 2
+#SBATCH -J 2D-NiC3
+#SBATCH -N 2 --exclusive
 #SBATCH --ntasks-per-node=16
-#SBATCH -t 004:00:00
-#SBATCH -A xxxxx
+#SBATCH -t 000:20:00
+#SBATCH -A naiss2025-5-112
 
-module add VASP/5.4.4.16052018-nsc1-intel-2018a-eb
-mpprun --compat el7 vasp_std
+module load VASP/5.4.4.16052018-nsc1-intel-2018a-eb
+mpprun vasp_xy_std
